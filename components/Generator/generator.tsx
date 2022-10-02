eed622c4c46a19e0c4bf23f472de5ccb6fb7a3f9
Ünal Parlak <unparlak@gmail.com>
2022-10-02 14:51:41 +0300

    dynamic import added

--- components/Generator/generator.tsx
+++ components/Generator/generator.tsx
@@ -1,5 +1,7 @@
 import styles from "./generator.module.scss";
 import Image from "next/future/image";
+import React, { lazy, useContext } from "react";
+import { Context } from "../../context/Context";
 import Gallery from "../Gallery/gallery";
 
 import Modal1 from "../ModalSamples/Modal1/modal1";
@@ -7,7 +9,19 @@ import Modal2 from "../ModalSamples/Modal2/modal2";
 import Modal4 from "../ModalSamples/Modal4/modal4";
 import Modal5 from "../ModalSamples/Modal5/modal5";
 
+import dynamic from "next/dynamic";
+
 function Generator() {
+  const { indexOfChosenModal } = useContext(Context);
+
+  function loadComponent(index: string) {
+    const Component = dynamic(
+      () => import(`../ModalSamples/Modal${index}/modal${index}.tsx`)
+    );
+    return Component;
+  }
+  const ChosenModal = loadComponent(indexOfChosenModal);
+
   return (
     <div className={styles.generator}>
       <h1>Modal Card Generator</h1>
@@ -18,7 +32,7 @@ function Generator() {
       </p>
       <Gallery />
 
-      <Modal5 />
+      <ChosenModal />
     </div>
   );
 }
