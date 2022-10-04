63d27eff3bd021b1ed5ced49205fe820b5790e1b
Ünal Parlak <unparlak@gmail.com>
2022-10-04 14:23:12 +0300

    context changed, modal preview and modal editing added

--- components/Generator/generator.tsx
+++ components/Generator/generator.tsx
@@ -1,18 +1,19 @@
 import styles from "./generator.module.scss";
 import Image from "next/future/image";
-import React, { lazy, useContext } from "react";
-import { Context } from "../../context/Context";
-import Gallery from "../Gallery/gallery";
-
-import Modal1 from "../ModalSamples/Modal1/modal1";
-import Modal2 from "../ModalSamples/Modal2/modal2";
-import Modal4 from "../ModalSamples/Modal4/modal4";
-import Modal5 from "../ModalSamples/Modal5/modal5";
-
 import dynamic from "next/dynamic";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../../context/Context";
+import { nanoid } from "nanoid";
+import Gallery from "../Gallery/gallery";
+import Appearance from "../Appearance/appearance";
 
 function Generator() {
-  const { indexOfChosenModal } = useContext(Context);
+  const {
+    indexOfChosenModal,
+    handleChange,
+    contentsOfModals,
+    setContentsOfModals,
+  } = useContext(Context);
 
   function loadComponent(index: string) {
     const Component = dynamic(
@@ -20,19 +21,52 @@ function Generator() {
     );
     return Component;
   }
-  const ChosenModal = loadComponent(indexOfChosenModal);
+
+  const PreviewModal = loadComponent(indexOfChosenModal);
+
+  function InputElements() {
+    let contentArr = [];
+    for (const [contentName, contentValue] of Object.entries(
+      contentsOfModals[`Modal${indexOfChosenModal}`]
+    )) {
+      contentArr.push(
+        <input
+          key={contentName}
+          type="text"
+          className="form-input"
+          name={contentName}
+          value={contentValue}
+          onChange={(e) => handleChange(e, indexOfChosenModal)}
+        />
+      );
+    }
+    return contentArr;
+  }
 
   return (
     <div className={styles.generator}>
       <h1>Modal Card Generator</h1>
-      <p>
+      <p className="w-2/5 tracking-tight mb-24">
         Measure your return on email marketing efforts easier and faster by
         using thebest online tools. Popupsmart is ready to help you build an
         efficient email list!
       </p>
-      <Gallery />
 
-      <ChosenModal />
+      <h3>
+        <span>1</span> Choose your template
+      </h3>
+      <Gallery />
+      <div className="flex justify-between mt-24">
+        <div>
+          <Appearance />
+          <h3 className="mb-7">
+            <span>3</span> Content
+          </h3>
+          <p>Edit your content</p>
+          <div className="flex flex-col">{InputElements()}</div>
+        </div>
+        <PreviewModal />
+      </div>
     </div>
   );
 }
