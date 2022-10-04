8be4e850f601a1ef6c098ca9b3bc40cc24708ef0
Ünal Parlak <unparlak@gmail.com>
2022-10-04 14:21:32 +0300

    context structure changed

--- components/ModalSamples/Modal4/modal4.tsx
+++ components/ModalSamples/Modal4/modal4.tsx
@@ -1,30 +1,41 @@
 import styles from "./modal4.module.scss";
 import { MdDeleteOutline } from "react-icons/md";
+import { useContext } from "react";
+import { Context } from "../../../context/Context";
 
 import PrimaryButton from "../../ModalComponents/PrimaryButton/primaryButton";
 import SecondaryButton from "../../ModalComponents/SecondaryButton/secondaryButton";
 
 function Modal4() {
+  const { contentsOfModals } = useContext(Context);
   return (
     <div className={styles.modal}>
       <div className="w-24 h-24 bg-primary_purple rounded-full flex justify-center items-center">
         <MdDeleteOutline className="w-12 h-12   text-white" />
       </div>
 
-      <h2 className="text-3xl font-bold mt-7 mb-5">Delete your profile</h2>
-      <p className="text-xl mb-7">
-        Your profile will be automatically deleted after 1 month.
-      </p>
+      <h2 className="text-3xl font-bold mt-7 mb-5">
+        {contentsOfModals.Modal4.content1}
+      </h2>
+      <p className="text-xl mb-7">{contentsOfModals.Modal4.content2}</p>
       <p className="text-gray-500 leading-tight">
-        You won’t be able to access to your profile after
-        <span className="text-black"> 30.08.2021</span>.
+        {contentsOfModals.Modal4.content3}
+        <span className="text-black"> {contentsOfModals.Modal4.content4}</span>.
       </p>
 
       <div className="  w-full mt-8">
         <PrimaryButton
-          value={{ text: "Delete my profile", className: "w-full mb-4" }}
+          value={{
+            text: `${contentsOfModals.Modal4.content5}`,
+            className: "w-full mb-4",
+          }}
         />
-        <SecondaryButton value={{ text: "Cancel", className: "w-full" }} />
+        <SecondaryButton
+          value={{
+            text: `${contentsOfModals.Modal4.content6}`,
+            className: "w-full",
+          }}
+        />
       </div>
     </div>
   );
--- components/ModalSamples/Modal5/modal5.tsx
+++ components/ModalSamples/Modal5/modal5.tsx
@@ -5,14 +5,19 @@ import { Context } from "../../../context/Context";
 import PrimaryButton from "../../ModalComponents/PrimaryButton/primaryButton";
 
 function Modal5() {
-  const { contentOfModal5, setContentOfModal5 } = useContext(Context);
+  const { contentsOfModals } = useContext(Context);
 
   return (
     <div className={styles.modal}>
-      <h2 className="text-3xl font-bold mb-5">{contentOfModal5.content1}</h2>
-      <p className="text-xl mb-7">{contentOfModal5.content2}</p>
+      <h2 className="text-3xl font-bold mb-5">
+        {contentsOfModals.Modal5.content1}
+      </h2>
+      <p className="text-xl mb-7">{contentsOfModals.Modal5.content2}</p>
       <PrimaryButton
-        value={{ text: `${contentOfModal5.content3}`, className: "w-full " }}
+        value={{
+          text: `${contentsOfModals.Modal5.content3}`,
+          className: "w-full ",
+        }}
       />
     </div>
   );
