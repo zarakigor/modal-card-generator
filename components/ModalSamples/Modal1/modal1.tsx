efd65eac902b575ddb51a4ef016f1365f0400f86
Ünal Parlak <unparlak@gmail.com>
2022-10-04 14:19:31 +0300

    context structure changed

--- components/ModalSamples/Modal1/modal1.tsx
+++ components/ModalSamples/Modal1/modal1.tsx
@@ -7,30 +7,33 @@ import { useContext } from "react";
 import { Context } from "../../../context/Context";
 
 function Modal1() {
-  const { contentOfModal1, setContentOfModal1 } = useContext(Context);
+  const { contentsOfModals } = useContext(Context);
   return (
     <div className={styles.modal}>
       <div className="w-24 h-24 bg-primary_purple rounded-full flex justify-center items-center">
         <RiShieldStarLine className="w-12 h-12   text-white" />
       </div>
       <h2 className="text-3xl font-bold mt-7 mb-5">
-        {contentOfModal1.content1}
+        {contentsOfModals.Modal1.content1}
       </h2>
-      <p className="text-xl mb-10">{contentOfModal1.content2}</p>
+      <p className="text-xl mb-10">{contentsOfModals.Modal1.content2}</p>
       <input
         type="input"
         className="form-input border-gray-300 rounded-lg w-full"
-        value={contentOfModal1.content3}
+        value={contentsOfModals.Modal1.content3}
       />
       <div className="flex justify-between w-full mt-8">
         <SecondaryButton
           value={{
-            text: `${contentOfModal1.content4}`,
+            text: `${contentsOfModals.Modal1.content4}`,
             className: "w-6/12 mr-3",
           }}
         />
         <PrimaryButton
-          value={{ text: `${contentOfModal1.content5}`, className: "w-6/12" }}
+          value={{
+            text: `${contentsOfModals.Modal1.content5}`,
+            className: "w-6/12",
+          }}
         />
       </div>
     </div>
