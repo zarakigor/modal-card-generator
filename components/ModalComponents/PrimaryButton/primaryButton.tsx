8478d8c26ec664beca48504aec73e8c8613b698a
Ünal Parlak <unparlak@gmail.com>
2022-10-05 19:02:00 +0300

    styles: text-white added

--- components/ModalComponents/PrimaryButton/primaryButton.tsx
+++ components/ModalComponents/PrimaryButton/primaryButton.tsx
@@ -2,13 +2,10 @@ import styles from "./primaryButton.module.scss";
 
 function PrimaryButton(props) {
   return (
-    <button
-      className={`${props.value.className} ${styles.btn}  bg-primary_purple`}
-    >
+    <button className={`${props.value.className} text-white ${styles.btn}`}>
       {props.value.text}
     </button>
   );
 }
 
 export default PrimaryButton;
-// className={styles.btn}
