a185cfd9f41bc05d0c3ce563d82c1607044892e9
Ünal Parlak <unparlak@gmail.com>
2022-10-05 22:03:31 +0300

    text-white removed

--- components/ModalComponents/PrimaryButton/primaryButton.tsx
+++ components/ModalComponents/PrimaryButton/primaryButton.tsx
@@ -2,7 +2,7 @@ import styles from "./primaryButton.module.scss";
 
 function PrimaryButton(props) {
   return (
-    <button className={`${props.value.className} text-white ${styles.btn}`}>
+    <button className={`${props.value.className} ${styles.btn}`}>
       {props.value.text}
     </button>
   );
