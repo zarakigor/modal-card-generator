4ae5940515afeeb18ab7c7df89ab6e37593f95b7
Ünal Parlak <unparlak@gmail.com>
2022-10-05 18:56:45 +0300

    removed unnecessary lines

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout/layout";
 import Link from "next/link";
-import { useContext } from "react";
-import { Context } from "../context/Context";
 import Header from "../components/Header/header";
 import Generator from "../components/Generator/generator";
 import Footer from "../components/Footer/footer";
-import { GetStaticProps } from "next";
 
 export default function Home() {
-  const { deneme } = useContext(Context);
   return (
     <Layout>
       <Head>
@@ -19,17 +15,6 @@ export default function Home() {
       <Header />
       <Generator />
       <Footer />
-
-      {/* <p>{deneme}</p> */}
     </Layout>
   );
 }
-
-// export const getStaticProps: GetStaticProps = async () => {
-//   const allPostsData = getSortedPostsData();
-//   return {
-//     props: {
-//       allPostsData,
-//     },
-//   };
-// };
