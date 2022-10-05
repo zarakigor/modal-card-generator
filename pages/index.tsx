e33735e616a2f3920022965867a8ee36ff2a6e9b
Ünal Parlak <unparlak@gmail.com>
2022-10-05 22:30:36 +0300

    poppins font is added to head

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,11 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <Header />
       <Generator />
